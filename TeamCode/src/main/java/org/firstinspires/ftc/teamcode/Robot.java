9ac9c23bf40133a81feb353e2f7c16308e445448
Adrian <adrianharischand@gmail.com>
2019-01-25 14:33:29 -0500

    initial teleop code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -6,23 +6,29 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.teamcode.systems.Arm;
 import org.firstinspires.ftc.teamcode.systems.DriveSystem;
 import org.firstinspires.ftc.teamcode.systems.Mechanism;
+import org.firstinspires.ftc.teamcode.systems.Vision;
 
 public class Robot extends Mechanism {
     public DriveSystem drive;
     public Arm arm;
+    public Vision vision;
 
     public Robot(LinearOpMode opMode) {
         this.opMode = opMode;
         drive = new DriveSystem(opMode);
         arm = new Arm(opMode);
+        vision = new Vision(opMode);
     }
 
     public Robot() {
         drive = new DriveSystem();
+        arm = new Arm(opMode);
     }
 
     @Override
     public void init(HardwareMap hwMap) {
         drive.init(hwMap);
+        arm.init(hwMap);
+        vision.init(hwMap);
     }
 }
