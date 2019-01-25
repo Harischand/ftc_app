82ea09ec296b5263ae5386395ab896aa7c3b534b
Adrian <adrianharischand@gmail.com>
2019-01-25 14:31:59 -0500

    initial teleop code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/Creater1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/Creater1.java
@@ -5,39 +5,36 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import org.firstinspires.ftc.teamcode.Robot;
 import org.firstinspires.ftc.teamcode.systems.Vision;
 
-@Autonomous(name = "Depo1")
-public class Depo1 extends AutoOpMode {
-    private Robot robot = new Robot(this);
+@Autonomous(name="Creater1")
+public class Creater1 extends AutoOpMode {
 
+    private Robot robot = new Robot(this);
     @Override
     public void initialize() {
         robot.init(hardwareMap);
         robot.arm.setCarriage(1);
-
     }
 
     @Override
-    void run() {
+    public void run() {
         if (opModeIsActive()) {
-           robot.arm.setClimberUp();
-           sleep(40);
-            robot.drive.drive(0,0,0);
+            robot.arm.setClimberUp();
+            sleep(40);
+            robot.drive.drive(0, 0, 0);
             sleep(200);
-            robot.drive.drive(0,0 , 1);
+            robot.drive.drive(0, 0, 1);
             sleep(200);
 
             robot.vision.trackMineralPosition();
             Vision.Position position = robot.vision.getPosition();
             if (position == Vision.Position.LEFT) {
-                robot.drive.drive(-1,0,0);
+                robot.drive.drive(-1, 0, 0);
                 sleep(1000);
             } else if (position == Vision.Position.RIGHT) {
                 //
             } else {
-                robot.drive.drive(0,1,0);
-                sleep(1000);
-               robot.arm.setMarkerDown(0.5);
-               sleep(100);
+
+
             }
         }
     }
