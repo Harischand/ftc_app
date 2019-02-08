d67da95909ba2ffccb42a3386f56ef7781585d80
Harischand <45468790+Harischand@users.noreply.github.com>
2019-02-08 18:28:14 -0500

    basic crater autonomous without climber.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/CreaterBackUp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/CreaterBackUp.java
@@ -19,17 +19,41 @@ public class CreaterBackUp extends AutoOpMode {
         if (opModeIsActive()) {
 
 
-            robot.arm.setClimberUp();
-            sleep(6600);
+            //robot.arm.setClimberUp();
+            //sleep(6600);
 
-            robot.drive.drive(0,0,0);
+            //robot.drive.drive(0,0,0);
+            //sleep(1000);
+
+            robot.drive.drive(0,-1,0);
             sleep(1000);
 
             robot.drive.drive(1,0,0);
             sleep(200);
-
-            robot.drive.drive(0,1,0);
+            
+            robot.drive.drive(0,0,1);
+            sleep(500);
+            
+            robot.drive.drive(-1,0,0);
+            sleep(150);
+            
+            robot.drive.drive(0,-1,0);
+            sleep(2400);
+            
+            robot.drive.drive(-1,0,0);
             sleep(200);
+            
+            robot.arm.setMarkerDown(0.5);
+            sleep(100);
+            
+            robot.drive.drive(0,0,1);
+            sleep(450);
+            
+            robot.drive.drive(1,0,0);
+            sleep(2300);
+            
+            robot.arm.setCarriage(1);
+            sleep(100);
         }
 
     }
