af72d500a4faab5a047539712bc356952480929c
Mahim <mahim216@gmail.com>
2019-02-14 21:44:43 -0500

    hopefully fixed the code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/CreaterBackUp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/CreaterBackUp.java
@@ -10,74 +10,23 @@ public class CreaterBackUp extends AutoOpMode {
     @Override
     void initialize() {
         robot.init(hardwareMap);
-
     }
 
     @Override
     void run() {
-        //            robot.arm.setClimberUp();
-        //            sleep(5500);
-
         runForTime(5.5, robot.arm::setClimberUp);
-
-        //            robot.drive.stop();
-        //            sleep(3000);
-
+        robot.arm.stopClimber();
         runForTime(3, robot.drive::stop);
-
-        //            robot.drive.drive(-1,0,0);
-        //            sleep(300);
-
         runForTime(.3, () -> robot.drive.drive(-1, 0, 0));
-
-        //            robot.drive.drive(0, -1, 0);
-        //            sleep(1300);
-
         runForTime(1.3, () -> robot.drive.drive(0, -1, 0));
-
-        //            robot.drive.drive(1, 0, 0);
-        //            sleep(700);
-
         runForTime(.7, () -> robot.drive.drive(1, 0, 0));
-
-        //            robot.drive.drive(0, 0, 1);
-        //            sleep(1200);
-
         runForTime(1.2, () -> robot.drive.drive(0, 0, 1));
-
-        //            robot.drive.drive(-1, 0, 0);
-        //            sleep(300);
-
         runForTime(.3, () -> robot.drive.drive(-1, 0, 0));
-
-        //            robot.drive.drive(0, -1, 0);
-        //            sleep(2800);
-
         runForTime(2.8, () -> robot.drive.drive(0, -1, 0));
-
-        //            robot.drive.drive(-1, 0, 0);
-        //            sleep(550);
-
         runForTime(.55, () -> robot.drive.drive(-1, 0, 0));
-
-        //            robot.arm.setMarkerDown(0.5);
-        //            sleep(100);
-
         runForTime(0.1, () -> robot.arm.setMarkerDown(0.5));
-
-        //            robot.drive.drive(0, 0, 1);
-        //            sleep(600);
-
         runForTime(.6, () -> robot.drive.drive(0, 0, 1));
-
-        //            robot.drive.drive(1, 0, 0);
-        //            sleep(1800);
-
         runForTime(1.8, () -> robot.drive.drive(1, 0, 0));
-
-        //            robot.arm.setCarriage(1);
-        //            sleep(100);
-
         runForTime(0.1, () -> robot.arm.setCarriage(1));
     }
 }
