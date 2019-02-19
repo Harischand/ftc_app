cee39a6e0d2fb7cdf3a4f58d2c3c6c6b7a22e359
Mahim <mahim216@gmail.com>
2019-02-18 23:15:29 -0500

    refactored

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/Creater1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/Creater1.java
@@ -12,7 +12,6 @@ public class Creater1 extends AutoOpMode {
     @Override
     public void initialize() {
         robot.init(hardwareMap);
-
     }
 
     @Override
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/TeleOpDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/TeleOpDrive.java
@@ -2,7 +2,6 @@ package org.firstinspires.ftc.teamcode.commands;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
 import org.firstinspires.ftc.teamcode.Robot;
 
 @TeleOp(name = "teleop")
@@ -16,20 +15,19 @@ public class TeleOpDrive extends OpMode {
 
     @Override
     public void loop() {
-        robot.drive.drive(-gamepad1.left_stick_y, -gamepad1.left_stick_x, -gamepad1.right_stick_x);
-         robot.arm.setArmMotor(-gamepad2.left_stick_y);
-         robot.arm.setInTakeMotor(gamepad2.left_trigger);
-         robot.arm.setOutTakeMotor(0.3*gamepad2.right_stick_y);
+        robot.drive.drive(
+                -gamepad1.left_stick_y, -gamepad1.left_stick_x,
+                -gamepad1.right_stick_x);
+        robot.arm.setArmMotor(-gamepad2.left_stick_y);
+        robot.arm.setInTakeMotor(gamepad2.left_trigger);
+        robot.arm.setOutTakeMotor(0.3 * gamepad2.right_stick_y);
         robot.arm.setCarriage(gamepad2.right_trigger);
-        
+
         if (gamepad2.dpad_up) {
             robot.arm.setClimberUp();
-        }
-
-        else if (gamepad2.dpad_down) {
+        } else if (gamepad2.dpad_down) {
             robot.arm.setClimberDown();
-        }
-        else if (gamepad2.dpad_right){
+        } else if (gamepad2.dpad_right) {
             robot.arm.stopClimber();
         }
 
