c44a5eee341404eede15ec0f2e563263aca0345a
Adrian <adrianharischand@gmail.com>
2019-02-19 19:32:54 -0500

    most of the code for depot side without vision but with a proper working climber using encoders

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/DepoBack_Up.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/DepoBack_Up.java
@@ -21,24 +21,33 @@ public class DepoBack_Up extends AutoOpMode {
 
         runForTime(0.15, () -> robot.drive.drive(-1,0,0));
 
-       runForTime(1.8, () -> robot.drive.drive(0,-1,0));
+       runForTime(0.3, () -> robot.drive.drive(0,-1,0));
+
+       runForTime(0.1, () -> robot.drive.drive(1,0,0));
+
+        runForTime(2.3, () -> robot.drive.drive(0,-1,0));
+
 
         runForTime(0.1, () -> robot.arm.setMarkerDown(-0.5));
 
-        runForTime(0.500, () -> robot.drive.drive(0,0,1));
+       runForTime(0.1, () -> robot.drive.drive(0,0,0.5));
 
-        runForTime(3.0, () -> robot.drive.drive(1,0,0));
+        //runForTime(3.0, () -> robot.drive.drive(1,0,0));
 
         /*
-        First latch off by moving climber up for 10 seconds,
+        First latch off by moving climber up using encoders,
 
         next move 150 milliseconds backwards
 
-        next strafing right 1.8 seconds,
+        next strafing right 0.3 seconds,
+
+        move forward for 0.1 seconds,
+
+        strafe for 2.3 seconds
 
         then drop marker into depot ,
 
-        then turn left for 0.5 seconds,
+        then turn left for 0.1 seconds,
 
         finally drive toward Crater for 3 seconds to partial park
 
