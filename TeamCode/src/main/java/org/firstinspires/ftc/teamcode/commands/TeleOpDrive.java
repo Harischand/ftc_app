8f60af0a49da781cb1519d32c0705e1d90653821
Harischand <45468790+Harischand@users.noreply.github.com>
2019-03-09 19:27:33 -0800

    comments to  explain code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/TeleOpDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/TeleOpDrive.java
@@ -7,6 +7,17 @@ import org.firstinspires.ftc.teamcode.Robot;
 @TeleOp(name = "teleop")
 public class TeleOpDrive extends OpMode {
     private Robot robot = new Robot();
+    
+    /* TeleOp code:
+    To drive forward and backward using left joystick y axis on gamepad1
+    to strafe use left joystick x axis on gamepad1, to turn use right joystick x axis on gamepad 1,
+    to move arm use left joystick y axis on gamepad2, 
+    to in take minerals use left trigger on gamepad2 
+    to out take use right joystick y axis on gamepad 2
+    to move carriage up or down use right trigger on gamepad2 
+    to move climber up or down use dpad up and down buttons on gamepad 2
+    
+    */
 
     @Override
     public void init() {
